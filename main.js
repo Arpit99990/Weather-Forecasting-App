57052a0da9a9ee1884cd6ca7621dfb2da5173c20
Interviewbit <arpittiwari621@gmail.com>
2025-07-13 12:35:36 +0530

    API fetch

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
-import { createElement } from 'react';
-import {myapi} from './api';
-const apikey = myapi();
-
+const apikey="8cd2ce489ba1cb0ab30db48fc3c929f6";
 window.addEventListener("load",()=>{
     if(navigator.geolocation){
-        navigator.geolocation.
-        getCurrentPosition((position)=>{
-            let lon = position.coords.longitude;
-            let lan = position.coords.latitude;
+        navigator.geolocation.getCurrentPosition((position)=>{
+            let lon= position.coords.longitude;
+            let lat= position.coords.latitude;
             const url= `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` + `lon=${lon}&appid=${apikey}`;
+            
 
-             fetch(url).then(res=>{
-                return res.json()
-             }).then((data)=>{
+            fetch(url).then((res)=>{
+                return res.json();
+            }).then((data)=>{
                 console.log(data);
-                weatherReport(data)
-             })
+                console.log(new Date().getTime())
+                var dat= new Date(data.dt)
+                console.log(dat.toLocaleString(undefined,'Asia/Kolkata'))
+                console.log(new Date().getMinutes())
+                weatherReport(data);
+            })
         })
     }
 })
+
 
 function searchByCity(){
     var place= document.getElementById('input').value;
@@ -34,83 +36,79 @@ function searchByCity(){
 }
 
 function weatherReport(data){
-    var urlcast=`http://api.openweathermap.org/data/2.5/forecast?q=${data.name}&appid=${apikey}`;
 
-    fetch(urlcast).then(res=>{
-                return res.json()
-             }).then((forecast)=>{
-                console.log(forecast);
-                hourForecast(forecast);
-                dayForecast(forecast);
+    var urlcast= `http://api.openweathermap.org/data/2.5/forecast?q=${data.name}&` + `appid=${apikey}`;
 
-                document.getElementById('city').
-                innerText=data.name+', '+ data.sys.country;
+    fetch(urlcast).then((res)=>{
+        return res.json();
+    }).then((forecast)=>{
+        console.log(forecast.city);
+        hourForecast(forecast);
+        dayForecast(forecast)
 
-                document.getElementById('temperature').
-                innerText=Math.floor(data.main.temp -273)+' °C';
+        console.log(data);
+        document.getElementById('city').innerText= data.name + ', '+data.sys.country;
+        console.log(data.name,data.sys.country);
+    
+        console.log(Math.floor(data.main.temp-273));
+        document.getElementById('temperature').innerText= Math.floor(data.main.temp-273)+ ' °C';
+    
+        document.getElementById('clouds').innerText= data.weather[0].description;
+        console.log(data.weather[0].description)
+        
+        let icon1= data.weather[0].icon;
+        let iconurl= "http://api.openweathermap.org/img/w/"+ icon1 +".png";
+        document.getElementById('img').src=iconurl
+    })
 
-                 document.getElementById('clouds').innerText= data.weather[0].description;
-                 console.log(data.weather[0].description)
-                 
-                 let icon1= data.weather[0].icon;
-                 let iconurl= "http://api.openweathermap.org/img/w/"+ icon1 +".png";
-                 document.getElementById('img').src=iconurl
-             })
-            }
+}
 
 function hourForecast(forecast){
-    document.querySelector('.templist').
-    innerHTML= '';
-    for(let i=0;i<5;i++){
-        var date=new Date(forecast.list[i].dt*1000);
+    document.querySelector('.templist').innerHTML=''
+    for (let i = 0; i < 5; i++) {
 
-        let houR=document.createElement('div');
-        houR.setAttribute('class','next');
+        var date= new Date(forecast.list[i].dt*1000)
+        console.log((date.toLocaleTimeString(undefined,'Asia/Kolkata')).replace(':00',''))
 
-        let div=document.createElement('div');
-        let time=document.createElement('p');
-        time.setAttribute('class','time');
-        time.innerText=(date.
-        toLocaleTimeString(undefined,'Asia/Kolkata')).replace(':00','');
-        
-        let temp=document.createElement('p');
-        temp.innerText=Math.floor(forecast.list[i].main.temp_max -273)+' °C'
-        +'/'+Math.floor(forecast.list[i].main.temp_min -273)+' °C';
+        let hourR=document.createElement('div');
+        hourR.setAttribute('class','next');
 
-        div.appendChild(time);
-        div.appendChild(temp);
+        let div= document.createElement('div');
+        let time= document.createElement('p');
+        time.setAttribute('class','time')
+        time.innerText= (date.toLocaleTimeString(undefined,'Asia/Kolkata')).replace(':00','');
 
-        let desc=document.createElement('p');
-        desc.setAttribute('class','desc');
-        desc.innerText=forecast.list[i].
-        weather[0].description;
+        let temp= document.createElement('p');
+        temp.innerText= Math.floor((forecast.list[i].main.temp_max - 273))+ ' °C' + ' / ' + Math.floor((forecast.list[i].main.temp_min - 273))+ ' °C';
 
-        houR.appendChild(div);
-        houR.appendChild(desc);
-        document.querySelector('.templist').
-        appendChild(houR);
-    }
+        div.appendChild(time)
+        div.appendChild(temp)
+
+        let desc= document.createElement('p');
+        desc.setAttribute('class','desc')
+        desc.innerText= forecast.list[i].weather[0].description;
+
+        hourR.appendChild(div);
+        hourR.appendChild(desc)
+        document.querySelector('.templist').appendChild(hourR);
 }
+}
+
 function dayForecast(forecast){
-    document.querySelector('weekF').
-    innerHTML='';
-
-    for(int =8;i<forecast.list.length;i+=8){
+    document.querySelector('.weekF').innerHTML=''
+    for (let i = 8; i < forecast.list.length; i+=8) {
         console.log(forecast.list[i]);
-        
-        let div=document.createElement('div');
+        let div= document.createElement('div');
         div.setAttribute('class','dayF');
-
-        let day=document.createElement('p');
-        day.setAttribute('class','date');
-        day.innerText=new Date(forecast.list[i].dt*1000).
-        toLocaleDateString(undefined,'Asia/Kolkata');
+        
+        let day= document.createElement('p');
+        day.setAttribute('class','date')
+        day.innerText= new Date(forecast.list[i].dt*1000).toDateString(undefined,'Asia/Kolkata');
         div.appendChild(day);
 
-        let temp=document.createElement('p');
-        temp.innerText=Math.floor(forecast.list[i].main.temp_max -273)+' °C'
-        +'/'+Math.floor(forecast.list[i].main.temp_min -273)+' °C';
-        div.appendChild(temp);
+        let temp= document.createElement('p');
+        temp.innerText= Math.floor((forecast.list[i].main.temp_max - 273))+ ' °C' + ' / ' + Math.floor((forecast.list[i].main.temp_min - 273))+ ' °C';
+        div.appendChild(temp)
 
         let description= document.createElement('p');
         description.setAttribute('class','desc')
@@ -119,4 +117,4 @@ function dayForecast(forecast){
 
         document.querySelector('.weekF').appendChild(div)
     }
-}
+} 
